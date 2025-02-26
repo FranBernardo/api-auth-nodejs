9f103d234ce0046c33d65a3a4ea1a243d459709e
Franciele <franciele.silva@fcamara.com.br>
2025-02-25 21:05:54 -0300

    feat: router exemples

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, Node.js com TypeScript!");
+import express, {Response, Request} from 'express'
+import { createServer } from 'http'
+
+
+const app = express()
+const PORT = 3000
+
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) =>{
+  res.send('Hello World!')
+}
+)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
